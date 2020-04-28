ebcfbfb9ebdb00a912391282f4ff3c74a5ba896d
rudyi95 <rud11@meta.ua>
2020-04-28 16:05:34 +0300

    Updates

---
+++ precache-manifest.61826700d02966c647be552437da1f64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c36dc7e276f87dda04ab4a83ff84b1e9",
+    "url": "/bootlegger/index.html"
+  },
+  {
+    "revision": "75a9746d585a68d6edbe",
+    "url": "/bootlegger/static/css/main.2d20b264.chunk.css"
+  },
+  {
+    "revision": "6f91b82ad4572045626e",
+    "url": "/bootlegger/static/js/2.d94e0245.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/bootlegger/static/js/2.d94e0245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75a9746d585a68d6edbe",
+    "url": "/bootlegger/static/js/main.d94b8539.chunk.js"
+  },
+  {
+    "revision": "d634330089611a40982f",
+    "url": "/bootlegger/static/js/runtime-main.86b4d5a0.js"
+  },
+  {
+    "revision": "b3136348714a32c7e5f5d5c5c09cb524",
+    "url": "/bootlegger/static/media/logo2.b3136348.png"
+  }
+]);
